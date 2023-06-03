b43d18cc9d86c92fffa1fb9afe0195d6da93d85e
xjy <xjy1907@163.com>
2023-06-03 15:37:03 +0800

    引入 vue-router 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 
 
 export  const App = defineComponent({
   setup() {
-    const refCount = ref(0)
-    const onClick = () => {
-      refCount.value++
-    }    
+    
     return () => <>
-    <div>
-      {refCount.value}
-    </div>
-    <div><button onClick={onClick}>+1</button></div>
+    <router-link to="/bar">1111</router-link>
+    <div><router-view></router-view></div>
     </>
+    
   }
 })
-
-
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import { App } from './App'
-
-
-createApp(App).mount('#app')
+import {createRouter, createWebHashHistory} from 'vue-router'
+import {Foo} from './views/Foo'
+import {Bar} from './views/Bar'
+const routes = [
+    {path:'/',component:Foo},
+    {path:'/bar',component:Bar}
+]
+const router = createRouter({
+    routes,
+    history: createWebHashHistory()
+})
+createApp(App).use(router).mount('#app')
